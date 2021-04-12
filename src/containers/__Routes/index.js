2fa235d9c8e5178ccbf14e7ab1a4c38fb95db141
Fatih Sultan Ay <f.sultanay@gmail.com>
2021-04-11 21:43:46 -0400

    service worker added

---
+++ src/containers/__Routes/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Route Setting
+import HomePage from "../HomePage";
+import HomePage2 from "../HomePage2";
+
+import AboutPage from "../AboutPage";
+import PorfolioGrid from "../PorfolioGrid";
+
+
+
+import ProjectDetails from "../ProjectDetails";
+
+
+import ContactPage from "../ContactPage";
+
+
+// Normal Route
+import PrivateRoute from "../_PrivateRoute";
+import PublicRoute from "../_PublicRoute";
+
+export default function Routes() {
+    return (
+        <Switch>
+            <PublicRoute
+                exact
+                path="/"
+                component={HomePage}
+            />
+            <PrivateRoute
+                path="/home"
+                component={HomePage}
+            />
+            {/* <PrivateRoute
+                path="/home2"
+                component={HomePage2}
+            /> */}
+          
+            <PrivateRoute
+                path="/about"
+                component={AboutPage}
+            />
+            <PrivateRoute
+                path="/protfolioGrid"
+                component={PorfolioGrid}
+            />
+           
+            <PrivateRoute
+                path="/projectDetails"
+                component={ProjectDetails}
+            />
+      
+            <PrivateRoute
+                path="/contact"
+                component={ContactPage}
+            />
+          
+            <Route exact component={HomePage} />
+        </Switch>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './containers/App/App';
+import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+import './css/font-awesome.min.css';
+import './css/flaticon.css';
+import './css/themify-icons.css';
+import './fonts/futura.css';
+import 'react-modal-video/scss/modal-video.scss';
+import "slick-carousel/slick/slick.css";
+import 'react-id-swiper/lib/styles/scss/swiper.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
